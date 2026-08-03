bcce466b380609bd6dbc6cfa83ab730453f58130
root <root@hyonte.com>
2026-08-03 08:07:16 +0000

    JNI build 2026-08-03 08:07:16

---
+++ jni/jni/clientlogic/CNetwork.cpp
@@ -0,0 +1,15 @@
+//rick was here
+#include "CNetwork.h"
+#include <stdint.h>
+
+#include "util/CJavaWrapper.h"
+
+const char* g_szServerNames[MAX_SERVERS] = {
+	"BBR | RED",
+	"BLACK RUSSIA | TEST"
+};
+
+const CSetServer::CServerInstanceEncrypted g_sEncryptedAddresses[MAX_SERVERS] = {
+	CSetServer::create("188.127.241.74", 1, 16, 3723, false),
+	CSetServer::create("188.127.241.74", 1, 16, 3723, false)	
+};
