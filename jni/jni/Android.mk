3624048e1f554a68e8d01da0d1e5d97a25788eb0
root <root@hyonte.com>
2026-08-15 18:33:05 +0000

    JNI build 2026-08-15 18:33:05

---
+++ jni/jni/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := fresh-connector
+LOCAL_LDLIBS := -llog
+
+# path
+FILE_LIST := $(wildcard $(LOCAL_PATH)/*.cpp)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/arm/*.cpp)
+FILE_LIST += $(LOCAL_PATH)/vendor/raknet/BitStream.cpp
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+                    $(LOCAL_PATH)/vendor/raknet
+
+LOCAL_CPPFLAGS := -w -s -fvisibility=hidden -pthread -Wall -fpack-struct=1 -O2 -std=c++14 -fexceptions
+LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2
+include $(BUILD_SHARED_LIBRARY)
