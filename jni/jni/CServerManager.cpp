f0aa4f8280c46d756f743c153de6f760ef323f47
root <root@hyonte.com>
2026-08-17 16:16:02 +0000

    JNI build 2026-08-17 16:16:02

---
+++ jni/jni/CServerManager.cpp
@@ -0,0 +1,16 @@
+﻿#include "CServerManager.h"
+
+#include <stdint.h>
+
+const char* g_szServerNames[MAX_SERVERS] = {
+	"{ff9729}[1]{ffffff} GOMM",
+};
+
+const CServerInstance::CServerInstanceEncrypted g_sEncryptedAddresses[MAX_SERVERS] = {
+	// �������
+	//CServerInstance::create("141.94.184.106", 1, 20, 1259, false), // 2 dev
+	//CServerInstance::create("141.94.184.106", 1, 20, 1259, false), // 1
+	CServerInstance::create("188.127.241.74", 1, 20, 2905, false), // 2 dev
+	//CServerInstance::create("141.94.184.106", 1, 14, 1259, false) // 1  Айпи СВОЙ IP
+	CServerInstance::create("188.127.241.74", 1, 20, 2905, false)
+};
