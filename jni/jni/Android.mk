a2421b990cfa1708a7faa3cba9e71000ac66aec8
root <root@hyonte.com>
2026-09-01 19:50:37 +0000

    Clean repository before build

--- jni/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := cheat
-LOCAL_SRC_FILES := main.cpp
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_CPP_FEATURES := exceptions rtti
-LOCAL_LDLIBS := -llog -lm -ldl
-LOCAL_CFLAGS := -O2 -fvisibility=hidden -fPIC
-include $(BUILD_SHARED_LIBRARY)
