42adf9849cdcacee9d88afde72b892f13dae4665
root <root@hyonte.com>
2026-09-02 23:27:22 +0000

    Clean repository before build

--- jni/jni/Application.mk
+++
@@ -1,9 +0,0 @@
-# Собираем только под arm64-v8a (под неё же и лежит папка lib/arm64-v8a)
-APP_ABI := arm64-v8a
-
-APP_PLATFORM := android-24
-APP_STL := c++_static
-APP_OPTIM := release
-
-# Имя модуля должно совпадать с LOCAL_MODULE из Android.mk
-APP_MODULES := apk
