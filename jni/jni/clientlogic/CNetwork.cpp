c0e90fc075d11300d4eb4836be19e6f85bab8d34
root <root@hyonte.com>
2026-09-04 17:45:56 +0000

    Auto-build 2026-09-04 17:45:56

---
+++ jni/jni/clientlogic/CNetwork.cpp
@@ -0,0 +1,17 @@
+// -- -- -- -- -- -- -- 
+// BLACK RUSSIA v1.2.1 by Merixton
+// -- -- -- -- -- -- --
+#include "CNetwork.h"
+#include <stdint.h>
+
+#include "util/CJavaWrapper.h"
+
+const char* g_szServerNames[MAX_SERVERS] = {
+	"BLACK MOSCOW | RED",
+	"BLACK RUSSIA | TEST"
+};
+
+const CSetServer::CServerInstanceEncrypted g_sEncryptedAddresses[MAX_SERVERS] = {
+	CSetServer::create("188.127.241.74", 1, 16, 2771, false),
+	CSetServer::create("188.127.241.74", 1, 16, 2771, false)	
+};
