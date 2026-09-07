ffdf9100539f4fdaae43956644ae20922c485cb8
root <root@hyonte.com>
2026-09-07 14:42:22 +0000

    Auto-build 2026-09-07 14:42:22

---
+++ jni/jni/clientlogic/BaseModelInfo.h
@@ -0,0 +1,21 @@
+//
+// Created by plaka on 21.02.2023.
+//
+
+#ifndef LIVERUSSIA_BASEMODELINFO_H
+#define LIVERUSSIA_BASEMODELINFO_H
+
+
+#include <cstdint>
+#include <cassert>
+
+struct CBaseModelInfo {
+
+            /* https://github.com/multitheftauto/mtasa-blue/blob/master/Client/game_sa/CModelInfoSA.h */
+
+};
+//static_assert(sizeof(CBaseModelInfo) == 0x38, "Invalid");
+// sizeof=0x38
+
+
+#endif //LIVERUSSIA_BASEMODELINFO_H
---
+++ jni/jni/clientlogic/CModelInfo.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "BaseModelInfo.h"
+
+class CModelInfo
+{
+private:
+    /* data */
+public:
+
+    static CBaseModelInfo **ms_modelInfoPtrs;
+
+};
