41b38748f8ce1ffaf2bad5ba8d50b080ee34ac43
root <root@hyonte.com>
2026-09-09 16:31:09 +0000

    Auto-build 2026-09-09 16:31:09

---
+++ jni/jni/app.h
@@ -0,0 +1,22 @@
+#pragma once
+
+enum eAppInit
+{
+	APP_INIT_OFFSETS,
+	APP_INIT_RW,
+};
+
+// t.me/gordeev_mods 
+// t.me/gordeev_mods 
+// t.me/gordeev_mods 
+// t.me/gordeev_mods 
+// t.me/gordeev_mods 
+// t.me/gordeev_mods 
+// t.me/gordeev_mods 
+// t.me/gordeev_mods 
+
+class CApp
+{
+public:
+	static void Initialise(eAppInit init_type);
+};
