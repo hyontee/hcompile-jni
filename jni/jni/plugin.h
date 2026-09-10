42cc8a0c50b8379c6495afe56d0185c8c2023f5e
root <root@hyonte.com>
2026-09-10 08:49:09 +0000

    Clean repository before build

--- jni/jni/plugin.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "vendor/Dobby/include/dobby.h"
-
-void hook_Gui(unsigned int pkt, int ll, const char* buffGUI);
-extern void (*orig_Gui)(unsigned int pkt, int ll, const char* buffGUI);
-void SetupHook();
