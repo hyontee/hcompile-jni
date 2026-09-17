58bdd29edc29dc3bd3cb162a379f60f07f99602f
root <root@hyonte.com>
2026-09-17 15:28:40 +0000

    Auto-build 2026-09-17 15:28:40

---
+++ jni/jni/game/CPlayerPed.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cstdint>
+
+#include "math/matrix.h"
+#include "math/vector.h"
+
+class CPlayerPed
+{
+public:
+	float GetWeaponRadiusOnScreen();
+	void TransformToNode(CVector* vec, int node);
+	uint8_t GetCurrentWeaponID();
+	void* GetCurrentWeapon();
+};
+
+CMatrix* GetBoneMatrix(CPlayerPed* ped, int bone);
