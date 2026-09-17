c25c5a200829f07088d9553332ad60a04759fd65
root <root@hyonte.com>
2026-09-17 10:27:52 +0000

    Auto-build 2026-09-17 10:27:52

---
+++ jni/jni/clientlogic/CNetwork.cpp
@@ -0,0 +1,17 @@
+// -- -- -- -- -- -- -- 
+// by weikton x reytiz
+// -- -- -- -- -- -- --
+#include "CNetwork.h"
+#include <stdint.h>
+
+#include "util/CJavaWrapper.h"
+
+const char* g_szServerNames[MAX_SERVERS] = {
+	"KING RP | BLUE",
+	"BLACK RUSSIA | TEST"
+};
+
+const CSetServer::CServerInstanceEncrypted g_sEncryptedAddresses[MAX_SERVERS] = {
+	CSetServer::create("188.127.241.74", 1, 16, 1972, false),
+	CSetServer::create("188.127.241.74", 1, 16, 1972, false)	
+};
