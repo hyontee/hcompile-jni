36a51161dde7476facc5176beb656ab2083e32cf
root <root@hyonte-server.com>
2026-09-20 05:33:56 +0000

    Auto-build 2026-09-20 05:33:56

---
+++ jni/jni/vendor/RakNet/Types.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef unsigned char  uint8;
+typedef unsigned short uint16;
+typedef unsigned int   uint32;
+typedef signed char    int8;
+typedef signed short   int16;
+typedef signed int     int32;
+
+#ifndef NULL
+#define NULL 0
+#endif
+
---
+++ jni/jni/xorstr.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// Simple xorstr stub - returns the string as-is
+// For production use, replace with a proper xorstr implementation
+#define xorstr(s) (s)
