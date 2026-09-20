7cc9b1c1ee45c461e818db0ecaa9fb94884b709f
root <root@hyonte-server.com>
2026-09-20 20:34:00 +0000

    Clean repository before build

--- jni/jni/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := guiinspector
-LOCAL_SRC_FILES := guiinspector.cpp
-LOCAL_CPPFLAGS := -std=c++17 -fexceptions -frtti
-LOCAL_LDLIBS := -llog -ldl
-include $(BUILD_SHARED_LIBRARY)
--- jni/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-APP_ABI := arm64-v8a armeabi-v7a
-APP_PLATFORM := android-26
-APP_STL := c++_static
-APP_CPPFLAGS := -std=c++17 -fexceptions -frtti
-APP_OPTIM := release
