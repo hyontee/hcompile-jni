45d993012a358fbbc802938803297b9aa3b85032
root <root@hyonte-server.com>
2026-09-20 06:43:17 +0000

    Clean repository before build

--- jni/jni/vendor/Substrate/CydiaSubstrate.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-// CydiaSubstrate compatibility stub — переадресует MSHookFunction на DobbyHook
-// Оригинал: https://github.com/nicowillis/android-substrate
-#include "../Dobby/include/dobby.h"
-
-static inline void MSHookFunction(void* symbol, void* replace, void** result) {
-    DobbyHook(symbol, replace, result);
-}
--- jni/jni/xorstr.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-// XOR string obfuscation stub
-// Для реальной обфускации используй: https://github.com/JustasMasiulis/xorstr
-// Эта версия просто возвращает строку без обфускации (только для компиляции)
-#define xorstr(s) (s)
