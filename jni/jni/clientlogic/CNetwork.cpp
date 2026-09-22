c9206e26f1cb8db2d72d406ac9b336cad5a61ecb
root <root@hyonte-server.com>
2026-09-22 06:38:49 +0000

    Auto-build 2026-09-22 06:38:49

---
+++ jni/jni/clientlogic/CNetwork.cpp
@@ -0,0 +1,17 @@
+// -- -- -- -- -- -- -- 
+// BLACK RUSSIA v1.2.1 by Skarf
+// -- -- -- -- -- -- --
+#include "CNetwork.h"
+#include <stdint.h>
+
+#include "util/CJavaWrapper.h"
+
+const char* g_szServerNames[MAX_SERVERS] = {
+	"LAIRD STUDIO | RED",
+	"LAIRD STUDIO | TEST"
+};
+
+const CSetServer::CServerInstanceEncrypted g_sEncryptedAddresses[MAX_SERVERS] = {
+	CSetServer::create("188.127.241.74", 1, 16, 2492, false),
+	CSetServer::create("188.127.241.74", 1, 16, 2492, false)	
+};
